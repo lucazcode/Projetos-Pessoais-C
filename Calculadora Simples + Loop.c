b3a95ce0bb58fd026b365573cf5cfbdaeeb7206c
Lucas Code <lucascode.ctt@gmail.com>
2024-07-08 16:50:21 -0300

    Calculadora Simples + Loop.c

--- Calculadora Simples + Loop.c
+++ Calculadora Simples + Loop.c
@@ -18,68 +18,50 @@ int main(){
 		while(loop2==1){
 				
 			printf("\ncalcular novamente? (1 = sim, 2 = não): ");
-		    scanf("%d", &escolha);
-		    fflush(stdin);
+		    	scanf("%d", &escolha);
+		    	fflush(stdin);
 		
 			if(escolha==1||escolha==2){	
-						
-				if(escolha==1){
-					
+				if(escolha==1){		
 					break;
-					
 				}	
 				
 				else{
-					
 					printf("encerrando.\n");
 					loop=0;
 					break;
-					
 				}
-				
 			}
 				
 			else{
-				
 				printf("escolha inválida, tente novamente.\n");
-				continue;	
-				
+				continue;		
 			}
-			
 		}
-		
 	}
 	
 	float soma(float numA, float numB){	
-	
 		float resultado = numA + numB;
 		printf("\nresultado: %.2f + %.2f = %.2f", numA, numB, resultado);
 		return resultado;
-		
 		}
 		
 	float sub(float numA, float numB){
-		
 		float resultado = numA - numB;
 		printf("\nresultado: %.2f - %.2f = %.2f", numA, numB, resultado);
 		return resultado;
-		
 	}
 	
 	float mult(float numA, float numB){
-		
 		float resultado = numA * numB;
 		printf("\nresultado: %.2f * %.2f = %.2f", numA, numB, resultado);
 		return resultado;
-		
 	}
 	
 	float div(float numA, float numB){
-		
 		float resultado = numA / numB;
 		printf("\nresultado: %.2f / %.2f = %.2f", numA, numB, resultado);
 		return resultado;
-		
 	}
 	
 	while(loop==1){
@@ -99,44 +81,31 @@ int main(){
 			fflush(stdin);
 			
 			if(op==1){
-				
 				soma(n1, n2);
 				loops();
-				
 			}
 			
 			else if(op==2){
-				
 				sub(n1, n2);
 				loops();
-				
 			}
 			
 			else if(op==3){
-				
 				mult(n1, n2);
 				loops();
-				
 			}
 			
 			else if(op==4){
-				
 				div(n1, n2);
 				loops();
-				
 			}
-			
 		}
 		
 		else{
-			
 			printf("ERRO: opção inválida.\n\n");
 			continue;
-			
 		}
-		
 	}
-	
 }
 
 
