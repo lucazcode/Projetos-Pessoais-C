ac2941ce918c1fd47ac3e9135c3161177fc12d8f
Lucas Code <lucascode.ctt@gmail.com>
2024-08-01 11:15:04 -0300

    Inversor de String.c

--- Inversor de String.c
+++ Inversor de String.c
@@ -7,19 +7,19 @@
 int main(int argc,char *argv[]){
 	setlocale(LC_ALL,"Portuguese");
 	
-	char palavra[N];
-	int i;
+	char palavra[N]; // Define uma variável char com tamanho N
+	int i; // Define uma variável int
 	
 	puts("INVERTA A STRING\n");	
 	printf("Digite a palavra: ");
-	scanf("%s[N]",&palavra);	
-	i=strlen(palavra);
+	scanf("%s[N]",&palavra); // Recebe um dado em string do usuário com limite N	
+	i=strlen(palavra); // Define i como a quantidade de caracteres da palavra
 	
 	printf("String invertida:");
 	
-	for(i=strlen(palavra);i>=0;i--){
-		printf("%c",palavra[i]);
-	}
+	for(i=strlen(palavra);i>=0;i--){ // Inicializa i na última posição da string, enquanto i for maior ou igual a 0 (primeira posição de string)...
+		printf("%c",palavra[i]); // Formate a letra da palavra de acordo com a posição de i
+	} // Subtraia i em 1
 
 	return 0;
 }
